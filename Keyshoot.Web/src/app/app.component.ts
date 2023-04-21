a5030c1aa9f3dab97ca27f3fbbabf1284dd49542
Paweł Potycz <spexords@gmail.com>
2023-04-21 19:35:41 +0200

    added test endpoint

--- Keyshoot.Web/src/app/app.component.ts
+++ Keyshoot.Web/src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'Keyshoot.Web';
+export class AppComponent implements OnInit {
+  text?: string;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http
+      .get('https://localhost:5000/test', { responseType: 'text' })
+      .subscribe((text) => (this.text = text));
+  }
 }
