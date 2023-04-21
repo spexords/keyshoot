0334f036b33abeace29f527e224efbd54f571d04
spexords <pawelek386@gmail.com>
2023-04-21 19:51:32 +0200

    Test endpoint (#1)
    * added test endpoint
    
    * created test controller
    
    ---------
    
    Co-authored-by: Paweł Potycz <spexords@gmail.com>

--- Keyshoot.Web/src/app/app.component.ts
+++ Keyshoot.Web/src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TestService } from './test.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'Keyshoot.Web';
+export class AppComponent implements OnInit {
+  text?: string;
+
+  constructor(private testService: TestService) {}
+
+  ngOnInit(): void {
+    this.testService.getTestMessage().subscribe((text) => (this.text = text));
+  }
 }
