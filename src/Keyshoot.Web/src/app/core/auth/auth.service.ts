13d7b761b9b422c6274e09c3c5204677e06a7a4a
Paweł Potycz <spexords@gmail.com>
2023-04-23 22:38:06 +0200

    moved auth config to environment

--- src/Keyshoot.Web/src/app/core/auth/auth.service.ts
+++ src/Keyshoot.Web/src/app/core/auth/auth.service.ts
@@ -1,47 +1,20 @@
 import { Injectable } from '@angular/core';
 import { AuthConfig, OAuthService } from 'angular-oauth2-oidc';
 import { BehaviorSubject, Observable } from 'rxjs';
-
-const authCodeFlowConfig: AuthConfig = {
-  issuer: 'https://localhost:7001',
-
-  // URL of the SPA to redirect the user to after login
-  redirectUri: 'http://localhost:4200',
-
-  // The SPA's id. The SPA is registerd with this id at the auth-server
-  // clientId: 'server.code',
-  clientId: 'web',
-
-  // Just needed if your auth server demands a secret. In general, this
-  // is a sign that the auth server is not configured with SPAs in mind
-  // and it might not enforce further best practices vital for security
-  // such applications.pp
-  // dummyClientSecret: 'secret',
-
-  responseType: 'id_token token',
-
-  // set the scope for the permissions the client should request
-  // The first four are defined by OIDC.
-  // Important: Request offline_access to get a refresh token
-  // The api scope is a usecase specific one
-  scope: 'openid api',
-
-  postLogoutRedirectUri: 'http://localhost:4200',
-
-  showDebugInformation: true,
-};
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
+  private _authCodeFlowConfig = environment.authConfig as AuthConfig;
   private _isAuthorizedSource: BehaviorSubject<boolean> = new BehaviorSubject(
     false
   );
   isAuthorized$: Observable<boolean> = this._isAuthorizedSource.asObservable();
 
   constructor(private oauthService: OAuthService) {
-    this.oauthService.configure(authCodeFlowConfig);
+    this.oauthService.configure(this._authCodeFlowConfig);
     this.oauthService.loadDiscoveryDocumentAndTryLogin().then(() => {
       this.oauthService.tryLoginImplicitFlow().then(() => {
         this._isAuthorizedSource.next(this.oauthService.hasValidAccessToken());
--- src/Keyshoot.Web/src/environments/environment.ts
+++ src/Keyshoot.Web/src/environments/environment.ts
@@ -1,4 +1,13 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://localhost:5001/api/'
+    apiUrl: 'https://localhost:5001/api/',
+    authConfig: {
+      issuer: 'https://localhost:7001',
+      redirectUri: 'http://localhost:4200',
+      clientId: 'web',
+      responseType: 'id_token token',
+      scope: 'openid api',
+      postLogoutRedirectUri: 'http://localhost:4200',
+      showDebugInformation: true,
+    }
 }
