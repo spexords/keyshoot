6dd89eefb715e2032d7d68092461a0683af5f40d
Paweł Potycz <spexords@gmail.com>
2023-05-06 18:51:49 +0200

    small styling improvements & leaderboard page

--- src/Keyshoot.Web/src/app/features/leaderboard/leaderboard.component.ts
+++ src/Keyshoot.Web/src/app/features/leaderboard/leaderboard.component.ts
@@ -1,11 +1,25 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-leaderboard',
   templateUrl: './leaderboard.component.html',
   styleUrls: ['./leaderboard.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LeaderboardComponent {
+export class LeaderboardComponent implements OnInit {
+  currentPage = 1;
+  form!: FormGroup;
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      player: [''],
+    });
+  }
+
+  onSubmit(): void {
+    console.log(this.form.value)
+  }
 }
--- src/Keyshoot.Web/src/app/features/leaderboard/leaderboard.module.ts
+++ src/Keyshoot.Web/src/app/features/leaderboard/leaderboard.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LeaderboardComponent } from './leaderboard.component';
 import { LeaderboardRoutingModule } from './leaderboard-routing.module';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 @NgModule({
-  declarations: [
-    LeaderboardComponent
-  ],
+  declarations: [LeaderboardComponent],
   imports: [
     CommonModule,
-    LeaderboardRoutingModule
-  ]
+    LeaderboardRoutingModule,
+    SharedModule,
+    MatPaginatorModule,
+  ],
 })
-export class LeaderboardModule { }
+export class LeaderboardModule {}
