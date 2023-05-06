e3bec967fc6b255c856436921e7add31bf126318
Paweł Potycz <spexords@gmail.com>
2023-05-06 13:40:29 +0200

    updated footer

--- src/Keyshoot.Web/src/app/core/footer/footer.component.ts
+++ src/Keyshoot.Web/src/app/core/footer/footer.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class FooterComponent {
+  icons = [
+    'assets/socials/instagram.svg',
+    'assets/socials/linkedin.svg',
+  ]
 
 }
